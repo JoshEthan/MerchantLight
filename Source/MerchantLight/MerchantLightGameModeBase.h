07b05c979383981895e4d1427c593bb8bebd5cf3
JoshEthan <joshuaeris@gmail.com>
2022-01-20 08:37:15 -0700

    Initial commit

---
+++ Source/MerchantLight/MerchantLightGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MerchantLightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MERCHANTLIGHT_API AMerchantLightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
